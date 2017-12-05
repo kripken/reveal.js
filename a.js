e4a3386f76dd6b19f5ff4cfb3b18337b00d54077
Alon Zakai (kripken) <alonzakai@gmail.com>
2017-12-04 16:50:50 -0800

    mandlebrot

---
+++ a.js
@@ -0,0 +1,171 @@
+// Mandlebrot runs a certain # of iterations
+const MAX_ITERS_PER_PIXEL = 100;
+
+// How many pixels to render per frame
+const PIXELS_PER_FRAME = 10;
+
+// The area to draw
+const UPPER_LEFT = { x: -2.5, y: -1.75 };
+const LOWER_RIGHT = { x: 1, y: 1.75 };
+
+// Create a module to work on
+var module = new Binaryen.Module();
+
+// Create a function type for  i32 (f32, f32)  (i.e., return i32, get two f32 params)
+var iff = module.addFunctionType('i', Binaryen.i32, [Binaryen.f32, Binaryen.f32]);
+
+// We receive two parameters, 0 and 1, and add two local variables,
+// 2 and 3, 4 and 5, for the current and next complex number,
+// and an integer counter, 6
+var body = module.block(
+  null,
+  [
+    module.loop('main-loop', module.block(null, [
+      // new value of the real portion
+      module.set_local(4,
+        module.f32.add(
+          module.f32.sub(
+            module.f32.mul(
+              module.get_local(2, Binaryen.f32),
+              module.get_local(2, Binaryen.f32)
+            ),
+            module.f32.mul(
+              module.get_local(3, Binaryen.f32),
+              module.get_local(3, Binaryen.f32)
+            )
+          ),
+          module.get_local(0, Binaryen.f32)
+        )
+      ),
+      // new value of the complex portion
+      module.set_local(5,
+        module.f32.add(
+          module.f32.mul(
+            module.f32.const(2),
+            module.f32.mul(
+              module.get_local(2, Binaryen.f32),
+              module.get_local(3, Binaryen.f32)
+            )
+          ),
+          module.get_local(1, Binaryen.f32)
+        )
+      ),
+      // if it is larger than 4 in squared norm, it escaped
+      module.if(
+        module.f32.ge(
+          module.f32.add(
+            module.f32.mul(
+              module.get_local(4, Binaryen.f32),
+              module.get_local(4, Binaryen.f32)
+            ),
+            module.f32.mul(
+              module.get_local(5, Binaryen.f32),
+              module.get_local(5, Binaryen.f32)
+            )
+          ),
+          module.f32.const(4)
+        ),
+        // return how many iterations it survived, brighter for
+        // longer
+        module.return(
+          module.get_local(6, Binaryen.i32)
+        )
+      ),
+      // if we reached the max iterations, stop
+      module.if(
+        module.i32.ge_u(
+          module.get_local(6, Binaryen.i32),
+          module.i32.const(MAX_ITERS_PER_PIXEL)
+        ),
+        module.return(
+          module.i32.const(0) // black, we did not diverge
+        )
+      ),
+      // keep going
+      module.set_local(6,
+        module.i32.add(
+          module.get_local(6, Binaryen.i32),
+          module.i32.const(1)
+        )
+      ),
+      module.set_local(2, module.get_local(4, Binaryen.f32)),
+      module.set_local(3, module.get_local(5, Binaryen.f32)),
+      module.br('main-loop')
+    ])),
+  ],
+  Binaryen.none
+);
+
+// Create the add function
+module.addFunction('mandelbrot', iff, [Binaryen.f32, Binaryen.f32, Binaryen.f32, Binaryen.f32, Binaryen.i32], body);
+
+// Export the function, so we can call it later (for simplicity we
+// export it as the same name as it has internally)
+module.addFunctionExport('mandelbrot', 'mandelbrot');
+
+// Print out the text
+console.log(module.emitText());
+
+if (!module.validate()) throw 'bad module :(';
+
+// Optimize the module! This adds tee_local, removes a return, etc.
+module.optimize();
+
+// Print out the optimized module's text
+console.log('optimized:\n\n' + module.emitText());
+
+// Get the binary in typed array form
+var binary = module.emitBinary();
+console.log('binary size: ' + binary.length);
+
+// We don't need the Binaryen module anymore, so we can tell it to
+// clean itself up
+module.dispose();
+
+// Compile the binary and create an instance
+var wasm = new WebAssembly.Instance(new WebAssembly.Module(binary), {})
+console.log(wasm); // prints something like "[object WebAssembly.Instance]"
+
+function draw() {
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+  var image = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  var data = image.data;
+  var pixels = [];
+  for (var x = 0; x < image.width; x++) {
+    for (var y = 0; y < image.height; y++) {
+      pixels.push({ x : x, y : y });
+    }
+  }
+  // fancy semi-shuffle
+  for (var i = 0; i < pixels.length; i++) {
+    var j = Math.floor(Math.random() * (pixels.length - i));
+    var t = pixels[i];
+    pixels[i] = pixels[j];
+    pixels[j] = t;
+  }
+  function iter() {
+    if (pixels.length === 0) return;
+    for (var i = 0; i < MAX_ITERS_PER_PIXEL && pixels.length > 0; i++) {
+      var pixel = pixels.pop();
+      var xFraction = pixel.x / image.width;
+      var yFraction = pixel.y / image.height;
+      var x = UPPER_LEFT.x + xFraction * (LOWER_RIGHT.x - UPPER_LEFT.x);
+      var y = UPPER_LEFT.y + yFraction * (LOWER_RIGHT.y - UPPER_LEFT.y);
+      var value = wasm.exports.mandelbrot(x, y) * 10;
+      var xPixel = Math.round(xFraction * image.width);
+      var yPixel = Math.round(yFraction * image.height);
+//alert(x + ' ' + y + '         ' + value);
+      var offset = 4 * (xPixel + yPixel * image.width); // RGBA
+      var color = Math.floor(value * 255 / MAX_ITERS_PER_PIXEL);
+      data[offset] = color;
+      data[offset + 1] = color;
+      data[offset + 2] = color;
+      data[offset + 3] = 255;
+    }
+    ctx.putImageData(image, 0, 0);
+    setTimeout(iter, 1000/60);
+  }
+  setTimeout(iter, 0);
+}
+
